04f27488875f4a047a314c5120802e54e41f8526
JoseMarshall PD <hebojosemar@gmail.com>
2021-07-25 14:14:45 +0100

    chore:  improve types

--- src/external/repositories/postgres/methods/create-entity.ts
+++ src/external/repositories/postgres/methods/create-entity.ts
@@ -4,7 +4,7 @@ import { MakeCreateEntityData } from '../postgres.types';
 // eslint-disable-next-line import/prefer-default-export
 export function makeCreateEntity<T>({ model }: MakeCreateEntityData) {
   return async (body: T) => {
-    const doc = await queryGuard(model.create(body));
-    return doc as T;
+    const doc = await queryGuard<T>(model.create(body));
+    return doc;
   };
 }
--- src/external/repositories/postgres/methods/get-one-entity.ts
+++ src/external/repositories/postgres/methods/get-one-entity.ts
@@ -5,7 +5,7 @@ import { MakeGetOneEntityData } from '../postgres.types';
 export function makeGetOneEntity<K>({ model, options }: MakeGetOneEntityData<K>) {
   return async (query: { id: string; includeDeleted?: string }) => {
     const { id, includeDeleted, ...q } = query;
-    const doc = await queryGuard(
+    const doc = await queryGuard<K>(
       model.findOne({
         where: includeDeleted ? { id, ...q } : { id, isDeleted: false, ...q },
         attributes: options.projection,
@@ -13,6 +13,6 @@ export function makeGetOneEntity<K>({ model, options }: MakeGetOneEntityData<K>)
       })
     );
 
-    return options.formatData ? options.formatData(doc) : (doc as K);
+    return options.formatData ? options.formatData(doc) : doc;
   };
 }
