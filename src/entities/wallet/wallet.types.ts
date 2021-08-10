ea73c5b3f0745b30414e516fbece9705a95cfb92
JoseMarshall <hebojosemar@gmail.com>
2021-08-10 13:21:18 +0100

    refactor: repo types

--- src/entities/wallet/wallet.types.ts
+++ src/entities/wallet/wallet.types.ts
@@ -1,9 +1,15 @@
-import { Wallet } from '../../constants';
+import { CashFlow, Wallet } from '../../constants';
 import { Entity } from '../entity.types';
 
 export interface IWallet extends Entity {
   [Wallet.Currency]: string;
   [Wallet.CurrentValue]: number;
+}
+
+export interface ITansferAmount {
+  [CashFlow.Receiver]: string;
+  [CashFlow.Sender]: string;
+  [CashFlow.Value]: string;
 }
 
 export type IWalletInput = Omit<IWallet, keyof Entity>;
