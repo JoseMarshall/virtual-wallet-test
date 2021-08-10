12e5c6be17d064a0a66597bd621c017a01d7c832
JoseMarshall <hebojosemar@gmail.com>
2021-08-10 15:32:38 +0100

    refactor: cash flow category `uc`

--- src/usecases/create-cash-flow-category/index.ts
+++ src/usecases/create-cash-flow-category/index.ts
@@ -1,19 +1,21 @@
 import { ApiErrorsName, ApiErrorsType } from '../../constants';
+import { makeCashFlowCategory } from '../../entities/cash-flow-category';
 import { ICashFlowCategoryInput } from '../../entities/cash-flow-category/cash-flow-category.types';
 import uow from '../../external/repositories/postgres/unit-of-work';
 import apiMessages from '../../locales/pt/api-server.json';
 import CustomError from '../../utils/custom-error';
 
 // eslint-disable-next-line import/prefer-default-export
-export function makeEditCashFlowCategoryUC() {
-  return async (req: { params: { id: string }; body: ICashFlowCategoryInput }) => {
+export function makeCashFlowCategoryUC() {
+  return async (data: ICashFlowCategoryInput) => {
     const unitOfWork = await uow();
-    try {
-      const payload = await unitOfWork
-        .makeCashFlowCategoryRepository()
-        .update(req.params, req.body);
 
-      return { payload };
+    try {
+      const cashFlowCategoryRepo = unitOfWork.makeCashFlowCategoryRepository();
+      const createdCategory = await cashFlowCategoryRepo.add(makeCashFlowCategory(data));
+      return {
+        payload: createdCategory,
+      };
     } catch (error) {
       throw error instanceof CustomError
         ? error
@@ -21,8 +23,8 @@ export function makeEditCashFlowCategoryUC() {
             statusCode: 422,
             name: ApiErrorsName.GenericName,
             type: ApiErrorsType.GenericType,
-            message: apiMessages['E-1004'],
-            i18nCode: 'E-1004',
+            message: apiMessages['E-1006'],
+            i18nCode: 'E-1006',
             stack: error.stack,
             details: error,
           });
--- src/usecases/get-one-cash-flow-category/index.ts
+++ src/usecases/get-one-cash-flow-category/index.ts
@@ -1,7 +1,33 @@
-import { ICashFlowCategoryRepository } from '../../external/repositories/repository.types';
+import { ApiErrorsName, ApiErrorsType } from '../../constants';
+import { ICashFlowCategory } from '../../entities/cash-flow-category/cash-flow-category.types';
+import { MakeGetOneEntityDependencies } from '../../external/repositories/postgres/postgres.types';
+import uow from '../../external/repositories/postgres/unit-of-work';
+import apiMessages from '../../locales/pt/api-server.json';
+import CustomError from '../../utils/custom-error';
 import { GetOneCashFlowCategory } from '../../validators/types/cash-flow-category';
 
 // eslint-disable-next-line import/prefer-default-export
-export function makeGetOneCashFlowCategoryUC(repository: ICashFlowCategoryRepository) {
-  return async (query: GetOneCashFlowCategory) => ({ payload: await repository.findOne(query) });
+export function makeGetOneCashFlowCategoryUC() {
+  return async (query: GetOneCashFlowCategory) => {
+    const unitOfWork = await uow();
+    try {
+      const payload = await unitOfWork
+        .makeCashFlowCategoryRepository()
+        .get<MakeGetOneEntityDependencies<ICashFlowCategory>>(query, {});
+
+      return { payload };
+    } catch (error) {
+      throw error instanceof CustomError
+        ? error
+        : new CustomError({
+            statusCode: 404,
+            name: ApiErrorsName.ResourceNotFound,
+            type: ApiErrorsType.GenericType,
+            message: apiMessages['E-1008'],
+            i18nCode: 'E-1008',
+            stack: error.stack,
+            details: error,
+          });
+    }
+  };
 }
